9f39c5933f101c7369644d16cc16b85e2f007d19
igordeagostin <igordeagostin@gmail.com>
2023-07-22 10:25:53 -0300

    Primeiro commit

---
+++ palestra-jb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace palestra_jb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
